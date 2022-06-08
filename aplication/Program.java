ff046a7991f4b6da10ef31df5b4f8d0a2a203bd4
albertinesilva-42 <albertinesilva_@hotmail.com>
2022-06-08 20:06:54 -0300

    Poo Composição

---
+++ aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import entities.OrderItem;
+import entities.Product;
+
+public class Program {
+
+    public static void main(String args[]){
+
+        Product p = new Product("TV", 1000.0);
+
+        OrderItem oi1 = new OrderItem(1, 1000.0, p);
+    }
+}
